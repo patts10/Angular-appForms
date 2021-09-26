3c77bfaf874ce260aacc3b77c3a77b200e6f4588
Christian Mendoza <jonathan.mendoza@bennu.cl>
2021-09-26 12:18:21 -0500

    create routes and lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template-routing.module').then( m => m.TemplateRoutingModule )
+  },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
