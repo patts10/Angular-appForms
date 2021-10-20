87ae608fe7dd89803bf231bb88a0ada6cd365bf1
Christian Mendoza <patts2810@gmail.com>
2021-10-19 21:54:07 -0500

    wip: create vlidations directory

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,50 +1,59 @@
 import { Component } from '@angular/core';
 
 interface MenuItem {
-  texto: string,
-  ruta: string
+  texto: string;
+  ruta: string;
 }
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styles: [
     `
-    li {
-      cursor: pointer;
-    }
-    `
-  ]
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class SidemenuComponent {
-
   templateMenu: MenuItem[] = [
     {
       texto: 'Básicos',
-      ruta: './template/basicos'
+      ruta: './template/basicos',
     },
     {
       texto: 'Dinámicos',
-      ruta: './template/dinamicos'
+      ruta: './template/dinamicos',
     },
     {
       texto: 'Switch',
-      ruta: './template/switches'
-    }
-  ]
-  
+      ruta: './template/switches',
+    },
+  ];
+
   reactiveMenu: MenuItem[] = [
     {
       texto: 'Básicos',
-      ruta: './reactive/basicos'
+      ruta: './reactive/basicos',
     },
     {
       texto: 'Dinámicos',
-      ruta: './reactive/dinamicos'
+      ruta: './reactive/dinamicos',
     },
     {
       texto: 'Switch',
-      ruta: './reactive/switches'
-    }
-  ]
+      ruta: './reactive/switches',
+    },
+  ];
 
+  authMenu: MenuItem[] = [
+    {
+      texto: 'Login',
+      ruta: './auth/login',
+    },
+    {
+      texto: 'Registro',
+      ruta: './auth/registro',
+    },
+  ];
 }
