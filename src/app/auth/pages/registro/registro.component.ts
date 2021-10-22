31985f1cb4aef410d043dc7e8bdab31951da103e
Christian Mendoza <patts2810@gmail.com>
2021-10-21 21:05:17 -0500

    wip: create asynchronous validations

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ValidatorService } from 'src/app/shared/validator/validator.service';
 import { noPuedeSePatts } from '../../../shared/validator/validaciones';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -15,22 +16,36 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: [, [ Validators.required, Validators.pattern(this.validatorService.nombreApellidoPatern) ] ],
-    email: [, [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ] ],
+    email: [, [ Validators.required, Validators.pattern(this.validatorService.emailPattern) ], [ this.emailValidator ] ],
     username: [, [ Validators.required, noPuedeSePatts ] ],
     password: [, [ Validators.required, Validators.minLength(6) ] ],
     password2: [, [ Validators.required ] ]
   }, {
     validators: [ this.validatorService.camposIguales('password', 'password2') ]
-  })
+  });
+
+  get emailErrorMsg(): string {
+    
+    const errors = this.miFormulario.get('email')?.errors;
+
+    if (errors?.required) return 'Email es obligatorio';
+    if (errors?.pattern) return 'El valor no tiene formato de email';
+    if (errors?.emailTomado) return 'Email ya ha sido tomado';
+
+    return '';
+  }
 
   constructor( private fb: FormBuilder,
-               private validatorService: ValidatorService) { }
+               private validatorService: ValidatorService,
+               private emailValidator: EmailValidatorService) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
-      nombre: 'Jona Mendoza',
+      nombre: 'Christian Mendoza',
       email: 'test1@test.com',
-      username: 'No puede ser pattsito'
+      username: 'patts10',
+      password: '123456',
+      password2: '123456'
     })
   }
 
